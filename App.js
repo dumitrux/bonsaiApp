cf0d82b6699834e445c91f67468d627cf5af8ff1
Marian Dumitru Danci <34145543+dumitrux@users.noreply.github.com>
2021-02-16 20:30:55 +0100

    add bottom nav bar, icons, fonts and colors

--- App.js
+++ App.js
@@ -1,12 +1,93 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import colors from './assets/colors/colors';
 
-const App = () => {
+
+function Feed() {
   return (
-    <View>
-      <Text>Hello, world!</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={styles.text}>Feed!</Text>
     </View>
   );
-};
+}
 
-export default App;
+function Profile() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+
+function Notifications() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="#B0DADA"
+      labelStyle={{ fontSize: 12 }}
+      barStyle={styles.bottomNav}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarLabel: 'Inicio',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarLabel: 'Itinerarios',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="timeline" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Hilos',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="clear-all" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  bottomNav: {
+    //backgroundColor: '#6FBBBB',
+    backgroundColor: colors.ColorPrincipal
+  },
+  text: {
+    fontFamily: 'Nunito-Bold'
+  }
+});
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
